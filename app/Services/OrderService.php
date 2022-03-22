414fe82796bbc1c734b8acaa60e93ca06d6b78a6
Pedro Guimarães <pedro.guimaraes@adventures.inc>
2022-03-22 14:43:15 -0300

    feature: order structure

---
+++ app/Services/OrderService.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Services;
+
+use App\Abstracts\AbstractModelService;
+
+class OrderService extends AbstractModelService
+{
+    protected function changeRepositoryHooks(): void {}
+}
